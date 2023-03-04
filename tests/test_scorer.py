c0d548953dbce2c8b3ae482ab13fe304f021b588
Sean MacAvaney <sean.macavaney@gmail.com>
2023-03-04 15:16:29 +0000

    finished test

--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -16,4 +16,15 @@ class TestBasic(unittest.TestCase):
           {'qid': '0', 'query': 'chemical reactions', 'docno' : 'd2', 'text' : 'chemistry society'},
           {'qid': '1', 'query': 'hello', 'docno' : 'd1', 'text' : 'hello there'},
         ]))
-        print(df)
+        self.assertAlmostEqual(0., df['score'][0])
+        self.assertAlmostEqual(11.133593, df['score'][1], places=5)
+        self.assertAlmostEqual(17.566324, df['score'][2], places=5)
+        self.assertEqual('0', df['qid'][0])
+        self.assertEqual('0', df['qid'][1])
+        self.assertEqual('1', df['qid'][2])
+        self.assertEqual('d1', df['docno'][0])
+        self.assertEqual('d2', df['docno'][1])
+        self.assertEqual('d1', df['docno'][2])
+        self.assertEqual(1, df['rank'][0])
+        self.assertEqual(0, df['rank'][1])
+        self.assertEqual(0, df['rank'][2])
